48dc84f9fe0596421202d7aa0b87655595ccd57a
prath <pratham007talukdar@gmail.com>
2026-02-10 19:27:48 +0530

    changed version name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = flutter.minSdkVersion
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.1"
     }
 
     buildTypes {
